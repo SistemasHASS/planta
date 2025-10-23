35fee555036884a739f6d3368a8320c1e8b9e799
rvegas92 <rvegas92@hotmail.com>
2025-10-23 11:09:44 -0500

    fix: planta base

---
+++ src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { LayoutComponent } from './modules/main/pages/layout/layout.component';
+import { ParametrosComponent } from './modules/main/pages/parametros/parametros.component';
+import { AuthGuard } from './modules/auth/guard/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    component: LayoutComponent,
+    children: [
+      { path: 'parametros', component: ParametrosComponent },
+      { path: '**', redirectTo: 'auth/login' }
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  }
+];
